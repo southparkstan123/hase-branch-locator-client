4e7152eec3babc6cfae0ec632ef8d14416573533
southparkstan123 <southparkstan123@gmail.com>
2024-03-28 00:24:14 +0800

    Disable the change language when data is loading

--- src/components/LanguageSwitcher.vue
+++ src/components/LanguageSwitcher.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="clearfix">
     <div class="float-right">
-      <span class="lang" @click="changeLanguage(item.value)" variant="link" v-bind:key="index"
+      <span :class="(isLoading === true) ? 'lang loading' : 'lang'" @click="changeLanguage(item.value)" variant="link" v-bind:key="index"
         v-for="(item, index) in selection">{{ item.label }}</span>
     </div>
   </div>
@@ -13,17 +13,20 @@ import { mapGetters } from 'vuex'
 export default {
   methods: {
     changeLanguage(val) {
-      const path = '/'
-      this.$store.dispatch('siteConfig/setLanguage', val, { root: true })
-      this.$store.dispatch('branchLocation/resetItems', { root: true })
-      const query = { 'lang': this.currentLanguage.value };
-      this.$router.replace({ path, query })
+      if(this.isLoading === false) {
+        const path = '/';
+        this.$store.dispatch('siteConfig/setLanguage', val, { root: true });
+        this.$store.dispatch('branchLocation/resetItems', { root: true });
+        const query = { 'lang': this.currentLanguage.value };
+        this.$router.replace({ path, query });
+      }
     }
   },
   computed: {
     ...mapGetters({
       selection: 'siteConfig/getLanguagesToBeSelected',
-      currentLanguage: 'siteConfig/getCurrentLanguage'
+      currentLanguage: 'siteConfig/getCurrentLanguage',
+      isLoading: 'branchLocation/getLoadingState'
     })
   }
 }
@@ -33,5 +36,10 @@ export default {
 .lang {
   padding: 5px;
   cursor: pointer;
+  opacity: 1;
+}
+
+.lang.loading {
+  opacity: 0.5;
 }
 </style>
